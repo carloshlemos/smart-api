a5eb0def17364bd1d07ccc98d95ea61602ff1ee4
Carlos Henrique Lemos <chenriquelemos@gmail.com>
2020-07-23 17:01:54 -0300

    Melhorias no OAuth2

--- src/main/java/br/gov/go/smart_api/config/ResourceServerConfig.java
+++ src/main/java/br/gov/go/smart_api/config/ResourceServerConfig.java
@@ -1,22 +1,28 @@
 package br.gov.go.smart_api.config;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableOAuth2Sso
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.requestMatcher(new RequestHeaderRequestMatcher("Authorization"))
+                .authorizeRequests()
                 .antMatchers("/", "/error**", "/v2/api-docs", "/configuration/ui", "/swagger-resources",
                         "/configuration/security", "/swagger-ui.html", "/webjars/**",
                         "/swagger-resources/configuration/ui", "/swagger-ui.html").permitAll()
-                .anyRequest().authenticated()
-                .and().logout().logoutUrl("/logout")
-                .logoutSuccessUrl("/");
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
     }
 }
